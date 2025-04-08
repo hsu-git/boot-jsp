99075d4b80cd76c9c12952bf5aa8fdf92734d9b2
hsu-git <hsu080206@gmail.com>
2025-04-08 10:49:53 +0900

    index.jsp 실행까지

--- src/main/java/org/example/bootjsp/BootJspApplication.java
+++ src/main/java/org/example/bootjsp/BootJspApplication.java
@@ -3,6 +3,7 @@ package org.example.bootjsp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Boot는 알아서 Scan을 돌려주는데 Application이 있는 하위 패키지에 돌려주는 개념...
 @SpringBootApplication
 public class BootJspApplication {
 
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>JSP</title>
+</head>
+<body>
+<p>Hello JSP!</p>
+</body>
+</html>
